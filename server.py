0f38ddbb1f1657f8a386a4dc353afb0d6c043295
Brett Holman <airminded@users.noreply.github.com>
2025-02-22 17:07:56 +1100

    Update server.py

--- server.py
+++ server.py
@@ -48,7 +48,7 @@ def bluesky_post(message, item):
     article_snippet = item['snippet']
     newspaper_title = item['title']['title']  # Updated to use the correct key
     date = arrow.get(item['date'], 'YYYY-MM-DD').format('D MMM YYYY')
-    embed_external = models.AppBskyEmbedExternal(
+    embed_external = models.AppBskyEmbedExternal.Main(
         external=models.AppBskyEmbedExternal.External(
             title=article_title,
             description=article_snippet,
@@ -56,10 +56,10 @@ def bluesky_post(message, item):
         )
     )
     post_with_link_card = bluesky_client.com.atproto.repo.create_record(
-        models.ComAtprotoRepoCreateRecord(
+        models.ComAtprotoRepoCreateRecord.Data(
             repo=bluesky_client.me.did,
             collection=models.ids.AppBskyFeedPost,
-            record=models.AppBskyFeedPost(
+            record=models.AppBskyFeedPost.Main(
                 created_at=bluesky_client.get_current_time_iso(),
                 text=message,
                 embed=embed_external
